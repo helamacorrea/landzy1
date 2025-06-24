12cc798e06e5bd5e49708f7c8491714a00c5698a
Helamã VPC <helamacorrea@gmail.com>
2025-06-23 21:52:39 -0300

    port update

--- app.py
+++ app.py
@@ -282,5 +282,7 @@ def schedule_visit():
         print(e)
         return jsonify({'status': 'error'}), 500
 
+port = int(os.environ.get("PORT", 10000))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=port)
